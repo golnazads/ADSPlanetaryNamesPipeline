81031b43f874e461c1aa68b10f20ddb608ca342f
golnazads <28757512+golnazads@users.noreply.github.com>
2024-06-09 10:10:05 -0400

    added couple of comments to add more commands for easier use

--- run.py
+++ run.py
@@ -61,7 +61,8 @@ def get_date(days):
 # python run.py -t Mars -n Amenthes -a add_keyword_to_knowledge_graph
 # python run.py -t Mars -n Amenthes -a remove_keyword_from_knowledge_graph
 # python run.py -t Mars -n Amenthes -a retrieve_identified_entities -c 0.75
-
+# TODO: add a command to run all the feature types for all celestial bodies for collect step
+# TODO: add a command to run all the feature names since d days ago
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Identify Planetary Nomenclature')
     parser.add_argument('-a', '--action', help='one of the followings: collect, predict, end_to_end (collect followed by predict).')
@@ -112,9 +113,7 @@ if __name__ == '__main__':
                                        PLANETARYNAMES_PIPELINE_ACTION.identify,
                                        PLANETARYNAMES_PIPELINE_ACTION.end_to_end]:
                         the_task = {'action_type': action_type, 'args': entity_args}
-                        # tasks.task_process_planetary_nomenclature.delay(the_task)
-                        print('---by passing queue for now')
-                        tasks.task_process_planetary_nomenclature(the_task)
+                        tasks.task_process_planetary_nomenclature.delay(the_task)
                     elif action_type == PLANETARYNAMES_PIPELINE_ACTION.remove_the_most_recent:
                         KBH_rows_deleted, KB_rows_deleted = app.remove_most_recent_knowledge_base_records(feature_name_entity=feature_name,
                                                                                                           target_entity=current_target)
